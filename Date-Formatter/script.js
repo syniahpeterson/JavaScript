f8ffba038a8eee3fb41c8b2239ddfd7669b73784
syniahpeterson <syniahpeterson@gmail.com>
2025-06-20 17:47:53 -0400

    Updated date formatter

--- Date-Formatter/script.js
+++ Date-Formatter/script.js
@@ -12,7 +12,6 @@ const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
 dateOptionsSelectElement.addEventListener("change", () => {
-
   switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
       currentDateParagraph.textContent = formattedDate
