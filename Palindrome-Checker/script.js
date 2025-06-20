92ea1826c0c1bc14d48e2f216dd1bb84c3632984
syniahpeterson <syniahpeterson@gmail.com>
2025-06-19 20:35:44 -0400

    Updated Palindrome Checker

--- Palindrome-Checker/script.js
+++ Palindrome-Checker/script.js
@@ -1,29 +1,39 @@
-const userInput = document.getElementById('text-input');
+const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
-// function to check for palindrome
-function palindromeChecker(input) {
-  if (input === "") {
-    alert("Please input a value");
-    return;
-  }
-  result.replaceChildren();
-  const upperInput = input.replace(/[^A-Za-z0-9]/gi, "").toUpperCase();
-  let message = `<strong>${input}</strong> ${upperInput === [...upperInput].reverse().join("") ? "is" : "is not"} a palindrome.`;
-  const statement = document.createElement("p");
-  statement.className = "user-input";
-  statement.innerHTML = message;
-  result.appendChild(statement);
-};
-// button click
+const container = document.getElementById("container");
+
 checkBtn.addEventListener("click", () => {
-  palindromeChecker(userInput.value);
-  userInput.value = "";
-});
-// enter key
-userInput.addEventListener("keydown", e => {
-  if (e.key === "Enter") {
-    palindromeChecker(userInput.value);
-    userInput.value = "";
+  if (textInput.value === "") {
+    alert("Please input a value");
+  } else {
+    isPalindrome(textInput.value);
+    textInput.value = "";
   }
 });
+
+textInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    if (textInput.value === "") {
+      alert("Please input a value");
+    } else {
+      isPalindrome(textInput.value);
+      textInput.value = "";
+    }
+  }
+});
+
+const isPalindrome = (str) => {
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversedStr = [...cleanedStr].reverse().join("");
+
+  if (cleanedStr === reversedStr) {
+    result.innerText = `${str} is a palindrome`;
+    result.style.color = "green";
+    container.style.boxShadow = "0 0 25px green";
+  } else {
+    result.innerText = `${str} is not a palindrome`;
+    result.style.color = "red";
+    container.style.boxShadow = "0 0 25px red";
+  }
+};
