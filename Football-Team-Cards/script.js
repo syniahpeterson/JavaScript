fdd2cb38102aa2aaa51df866f44cee02afcfe18a
syniahpeterson <syniahpeterson@gmail.com>
2025-06-26 17:24:38 -0400

    Updated script.js

--- Football-Team-Cards/script.js
+++ Football-Team-Cards/script.js
@@ -28,7 +28,7 @@ const myFavoriteFootballTeam = {
       isCaptain: false,
       nickname: null,
     },
-     {
+    {
       name: "Ricardo Bochini",
       position: "midfielder",
       number: 3,
@@ -182,17 +182,16 @@ headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
   playerCards.innerHTML += arr
-    .map(
-      ({ name, position, number, isCaptain, nickname }) => {
-        return `
+    .map(({ name, position, number, isCaptain, nickname }) => {
+      return `
         <div class="player-card">
           <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
           <p>Position: ${position}</p>
           <p>Number: ${number}</p>
           <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>
-      ` }
-    )
+      `;
+    })
     .join("");
 };
 
@@ -222,8 +221,7 @@ playersDropdownList.addEventListener("change", (e) => {
       );
       break;
 
-  default:
-    setPlayerCards();
-
+    default:
+      setPlayerCards();
   }
 });
