0c2141b1854ca133ab248ebe160bdc5fe89937d0
syniahpeterson <syniahpeterson@gmail.com>
2025-06-27 16:35:54 -0400

    Cleaned special chars

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -13,7 +13,9 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
-const removeSpecialChars = (str) => str.replace(/[^a-zA-Z0-9\s]/g, "");
+const removeSpecialChars = (val) => {
+  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, "");
+};
 
 const addOrUpdateTask = () => {
   if (!titleInput.value.trim()) {
@@ -22,10 +24,13 @@ const addOrUpdateTask = () => {
   }
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    title: titleInput.value,
+    id: `${removeSpecialChars(titleInput.value)
+      .toLowerCase()
+      .split(" ")
+      .join("-")}-${Date.now()}`,
+    title: removeSpecialChars(titleInput.value),
     date: dateInput.value,
-    description: descriptionInput.value,
+    description: removeSpecialChars(descriptionInput.value),
   };
 
   if (dataArrIndex === -1) {
