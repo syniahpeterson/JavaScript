c3fd1a79fc23a9b60bd1b6571647ee97a18baeba
syniahpeterson <syniahpeterson@gmail.com>
2025-07-02 12:45:24 -0400

    Updated Telephone Number Validator

--- Telephone-Number-Validator/script.js
+++ Telephone-Number-Validator/script.js
@@ -1,46 +1,49 @@
-// Variable declarations from html elements
-const userInput = document.getElementById("user-input");
+const input = document.getElementById("user-input");
+const results = document.getElementById("results-div");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
-const results = document.getElementById("results-div");
 
-// Function that check if the number entered is valid
-function numberChecker(input) {
-  if (input === "") {
-    alert("Please provide a phone number");
-    return;
-  }
-  const country = '^(1\\s?)?';
-  const area = '(\\([0-9]{3}\\)|[0-9]{3})';
-  const dashes = '[\\s\\-]?';
-  const number = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
-  const regex = new RegExp(`${country}${area}${dashes}${number}`);
-
-// The result of the number entered is displayed
-if(regex.test(input)){
-  results.innerText += "Valid US number: " + input + "\n";
-}
-else {
-  results.innerText += "Invalid US number: " + input + "\n";
-}
-};
-
-// Checks for clicks of the checkBtn to start program
 checkBtn.addEventListener("click", () => {
-  numberChecker(userInput.value);
-  results.value += userInput;
-  userInput.value = "";
-});
-
-// Checks for click of enter key to start program
-userInput.addEventListener("keydown", e => {
-  if (e.key === "Enter") {
-    numberChecker(userInput.value);
-    userInput.value = "";
+  if (input.value === "") {
+    alert("Please provide a phone number");
+  } else {
+    phoneNumberValidator(input.value);
+    results.classList.remove("hidden");
+    results.classList.add("showing");
   }
 });
 
-// Checks for click of clearBtn to clear results div
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    if (input.value === "") {
+      alert("Please provide a phone number");
+    } else {
+      phoneNumberValidator(input.value);
+      results.classList.remove("hidden");
+      results.classList.add("showing");
+    }
+  }
+});
+
 clearBtn.addEventListener("click", () => {
-  results.innerHTML = "";
-})
+  results.textContent = "";
+  results.classList.remove("showing");
+  results.classList.add("hidden");
+});
+
+const phoneNumberValidator = (number) => {
+  input.value = "";
+  const phoneRegex =
+    /^(?:\+?1[\s-]?)?(?:\([0-9]{3}\)|[0-9]{3})[\s-]?[0-9]{3}[\s-]?[0-9]{4}$/;
+  if (phoneRegex.test(number)) {
+    const p = document.createElement("p");
+    p.textContent = "Valid US number: " + number;
+    p.style.color = "#10b981";
+    results.appendChild(p);
+  } else {
+    const p = document.createElement("p");
+    p.textContent = "Invalid US number: " + number;
+    p.style.color = "#ef4444";
+    results.appendChild(p);
+  }
+};
